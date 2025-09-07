8748b51f9fc396e140b845a9997bfa30203f750e
Gary Firzon <gfirzon@yahoo.com>
2025-09-07 12:00:33 -0500

    continue work

--- core/sidebar.py
+++ core/sidebar.py
@@ -5,7 +5,10 @@ from utils.time import get_formatted_datetime
 def create_sidebar():
     st.sidebar.title("Sidebar")
     image_path = os.path.join(os.getcwd(), 'static', 'me.png')
-    st.sidebar.image(image_path, use_container_width=False, width=200)
+    #st.sidebar.image(image_path, width='stretch')
+    st.sidebar.image(image_path, width='content')
+    #st.sidebar.image(image_path, use_container_width=False, width=200)
+    #st.sidebar.image(image_path, use_container_width=False, width=200)
 
     st.sidebar.write("This is the sidebar.")
     st.sidebar.write(get_formatted_datetime())
